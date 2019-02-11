9a7e880f41c5e795d37c0e00f9efb1d2df642683
Alexandre Bury <alexandre.bury@gmail.com>
2019-02-10 16:17:59 -0800

    Cargo fmt

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,23 +1,20 @@
 // Crate Dependencies ---------------------------------------------------------
 // ----------------------------------------------------------------------------
-extern crate rand;
 extern crate cursive;
 extern crate cursive_table_view;
-
+extern crate rand;
 
 // STD Dependencies -----------------------------------------------------------
 // ----------------------------------------------------------------------------
 use std::cmp::Ordering;
 
-
 // External Dependencies ------------------------------------------------------
 // ----------------------------------------------------------------------------
-use rand::Rng;
-use cursive::Cursive;
-use cursive::traits::*;
 use cursive::align::HAlign;
+use cursive::traits::*;
 use cursive::views::{Dialog, TextView};
-
+use cursive::Cursive;
+use rand::Rng;
 
 // Modules --------------------------------------------------------------------
 // ----------------------------------------------------------------------------
@@ -27,7 +24,7 @@ use cursive_table_view::{TableView, TableViewItem};
 enum BasicColumn {
     Name,
     Count,
-    Rate
+    Rate,
 }
 
 impl BasicColumn {
@@ -35,7 +32,7 @@ impl BasicColumn {
         match *self {
             BasicColumn::Name => "Name",
             BasicColumn::Count => "Count",
-            BasicColumn::Rate => "Rate"
+            BasicColumn::Rate => "Rate",
         }
     }
 }
@@ -44,45 +41,49 @@ impl BasicColumn {
 struct Foo {
     name: String,
     count: usize,
-    rate: usize
+    rate: usize,
 }
 
 impl TableViewItem<BasicColumn> for Foo {
-
     fn to_column(&self, column: BasicColumn) -> String {
         match column {
             BasicColumn::Name => self.name.to_string(),
             BasicColumn::Count => format!("{}", self.count),
-            BasicColumn::Rate => format!("{}", self.rate)
+            BasicColumn::Rate => format!("{}", self.rate),
         }
     }
 
-    fn cmp(&self, other: &Self, column: BasicColumn) -> Ordering where Self: Sized {
+    fn cmp(&self, other: &Self, column: BasicColumn) -> Ordering
+    where
+        Self: Sized,
+    {
         match column {
             BasicColumn::Name => self.name.cmp(&other.name),
             BasicColumn::Count => self.count.cmp(&other.count),
-            BasicColumn::Rate => self.rate.cmp(&other.rate)
+            BasicColumn::Rate => self.rate.cmp(&other.rate),
         }
     }
-
 }
 
 fn main() {
-
     let mut rng = rand::thread_rng();
 
     let mut siv = Cursive::default();
     let mut table = TableView::<Foo, BasicColumn>::new()
         .column(BasicColumn::Name, "Name", |c| c.width_percent(20))
         .column(BasicColumn::Count, "Count", |c| c.align(HAlign::Center))
-        .column(BasicColumn::Rate, "Rate", |c| c.ordering(Ordering::Greater).align(HAlign::Right).width_percent(20));
+        .column(BasicColumn::Rate, "Rate", |c| {
+            c.ordering(Ordering::Greater)
+                .align(HAlign::Right)
+                .width_percent(20)
+        });
 
     let mut items = Vec::new();
     for i in 0..50 {
         items.push(Foo {
             name: format!("Name {}", i),
             count: rng.gen_range(0, 255),
-            rate: rng.gen_range(0, 255)
+            rate: rng.gen_range(0, 255),
         });
     }
 
@@ -91,36 +92,33 @@ fn main() {
     table.set_on_sort(|siv: &mut Cursive, column: BasicColumn, order: Ordering| {
         siv.add_layer(
             Dialog::around(TextView::new(format!("{} / {:?}", column.as_str(), order)))
-                   .title("Sorted by")
-                   .button("Close", |s| drop(s.pop_layer())) // drop to avoid compile-time error 
+                .title("Sorted by")
+                .button("Close", |s| {
+                    s.pop_layer();
+                }),
         );
     });
 
     table.set_on_submit(|siv: &mut Cursive, row: usize, index: usize| {
-
-        let value = siv.call_on_id("table", move |table: &mut TableView<Foo, BasicColumn>| {
-            format!("{:?}", table.borrow_item(index).unwrap())
-
-        }).unwrap();
+        let value = siv
+            .call_on_id("table", move |table: &mut TableView<Foo, BasicColumn>| {
+                format!("{:?}", table.borrow_item(index).unwrap())
+            })
+            .unwrap();
 
         siv.add_layer(
             Dialog::around(TextView::new(value))
-                   .title(format!("Removing row # {}", row))
-                   .button("Close", move |s| {
-                        s.call_on_id("table", |table: &mut TableView<Foo, BasicColumn>| {
-                            table.remove_item(index);
-                        });
-                        drop(s.pop_layer()) // drop to avoid compile-time error
-                   })
+                .title(format!("Removing row # {}", row))
+                .button("Close", move |s| {
+                    s.call_on_id("table", |table: &mut TableView<Foo, BasicColumn>| {
+                        table.remove_item(index);
+                    });
+                    s.pop_layer();
+                }),
         );
-
     });
 
-    siv.add_layer(
-        Dialog::around(table.with_id("table").min_size((50, 20))).title("Table View")
-    );
+    siv.add_layer(Dialog::around(table.with_id("table").min_size((50, 20))).title("Table View"));
 
     siv.run();
-
 }
-
